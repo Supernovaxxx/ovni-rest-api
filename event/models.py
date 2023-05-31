697552a9ef74d579b7dc9b8cbeed763ab5fb7e7b
Joao Marcos Zuneda Righetto <JM@Jessicas-MacBook-Air.local>
2023-05-31 14:21:26 -0300

    create event model

--- event/models.py
+++ event/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=255)
+    subtitle = models.CharField(max_length=255)
+    start_date = models.DateTimeField(auto_now=False, auto_now_add=False)
+    end_date = models.DateTimeField(auto_now=False, auto_now_add=False)
