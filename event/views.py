31dd9233513541fd4d2e0923b3a6f966ef9699ac
Joao Marcos Zuneda Righetto <JM@Jessicas-MacBook-Air.local>
2023-06-01 09:52:53 -0300

    fix: change EventViewSet to EventList in views

--- event/views.py
+++ event/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
 
 from .models import Event
-from rest_framework import viewsets, permissions
+from rest_framework import permissions, generics
 from .serializers import EventSerializer
 
 
-class EventViewSet(viewsets.ModelViewSet):
+class EventList(generics.ListCreateAPIView):
     """
-    API endpoint that allows events to be viewed or edited.
+    API endpoint that allows events to be viewed or created.
     """
     queryset = Event.objects.all().order_by('-start_date')
     serializer_class = EventSerializer
     permission_classes = [permissions.IsAuthenticated]
-
