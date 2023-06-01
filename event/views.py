0902e3e2995878e54c65994497c4eaa07a4e9201
Joao Marcos Zuneda Righetto <JM@Jessicas-MacBook-Air.local>
2023-05-31 22:00:31 -0300

    create event list endpoint

--- event/views.py
+++ event/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Event
+from rest_framework import viewsets, permissions
+from .serializers import EventSerializer
+
+
+class EventViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows events to be viewed or edited.
+    """
+    queryset = Event.objects.all().order_by('-start_date')
+    serializer_class = EventSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
