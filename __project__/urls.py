c77643c0a1f69a5b15bef821bb5119824a0c3c28
Joao Marcos Zuneda Righetto <JM@Jessicas-MacBook-Air.local>
2023-06-01 11:14:14 -0300

    fix: set  urls for EventList and EventDetail views

--- __project__/urls.py
+++ __project__/urls.py
@@ -1,17 +1,15 @@
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework import routers
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from event import views
 
-
-router = routers.DefaultRouter()
-router.register('events', views.EventViewSet)
-
-
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('', admin.site.urls),
+    path('events/', views.EventList.as_view()),
+    path('events/<int:pk>/', views.EventDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
 
 urlpatterns += staticfiles_urlpatterns()
