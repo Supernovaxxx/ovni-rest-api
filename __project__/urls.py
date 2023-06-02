44938892475ede15387886959ace05fad3fd147b
Joao Marcos Zuneda Righetto <JM@Jessicas-MacBook-Air.local>
2023-06-02 18:16:14 -0300

    reorganize urls files

--- __project__/urls.py
+++ __project__/urls.py
@@ -1,6 +1,9 @@
+from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
+    path('', admin.site.urls),
     path('', include('event.urls')),
     path('', include('rest_framework.urls')),
 ]
