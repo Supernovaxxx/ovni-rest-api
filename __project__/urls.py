e1c1122263d85e0f55f3e10a262835b40178b5fb
Joao Marcos Zuneda Righetto <JM@Jessicas-MacBook-Air.local>
2023-06-01 22:06:06 -0300

    reorganize url files

--- __project__/urls.py
+++ __project__/urls.py
@@ -1,15 +1,6 @@
-from django.contrib import admin
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
-from event import views
+from django.urls import path, include
 
 urlpatterns = [
-    path('', admin.site.urls),
-    path('events/', views.EventList.as_view()),
-    path('events/<int:pk>/', views.EventDetail.as_view()),
+    path('', include('event.urls')),
+    path('', include('rest_framework.urls')),
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
-
-urlpatterns += staticfiles_urlpatterns()
