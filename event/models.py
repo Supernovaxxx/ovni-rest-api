e700f3940862bb362466fcc46267f6722cc37de3
Joao Marcos Zuneda Righetto <JM@Jessicas-MacBook-Air.local>
2023-06-01 21:56:29 -0300

    remove unnecessary code from event model

--- event/models.py
+++ event/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Event(models.Model):
     title = models.CharField(max_length=255)
     subtitle = models.CharField(max_length=255)
-    start_date = models.DateTimeField(auto_now=False, auto_now_add=False)
-    end_date = models.DateTimeField(auto_now=False, auto_now_add=False)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
