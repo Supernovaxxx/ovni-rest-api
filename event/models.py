99f69875a3aedc5284e79517111026cae263b7f6
Joao Marcos <jm_zr@hotmail.com>
2023-06-07 12:51:07 -0300

    feat: create Event model check constraints

--- event/models.py
+++ event/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models import Q, F
+
+from datetime import datetime, timedelta
 
 
 class Event(models.Model):
@@ -9,3 +12,15 @@ class Event(models.Model):
 
     class Meta:
         ordering = ["-start_date"]
+        constraints = [ # TODO determine actual business constraints
+            models.CheckConstraint(check=Q(start_date__gt=datetime.now()+timedelta(days=1)),
+                                   name="start_date_gt_one_day"),
+
+            models.CheckConstraint(check=Q(end_date__gt=datetime.now()+timedelta(days=2)),
+                                   name="end_date_gt_two_days"),
+
+            models.CheckConstraint(check=Q(end_date__gt=(F("start_date")+timedelta(days=1))), 
+                                   name="duration_at_least_one_day"),
+        ]
+
+
