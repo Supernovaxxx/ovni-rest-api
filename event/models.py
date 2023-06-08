22b1a2c2068e8f75acee100e3216705829580f03
Joao Marcos <jm_zr@hotmail.com>
2023-06-08 11:43:16 -0300

    fix: remove unnecessary constraints in Event model

--- event/models.py
+++ event/models.py
@@ -5,11 +5,6 @@ from django.utils import timezone
 from datetime import timedelta
 
 
-class ActiveEventManager(models.Manager):
-    def get_queryset(self):
-        return super().get_queryset().filter(Q(start_date__gt=timezone.now()))
-        
-
 class Event(models.Model):
     title = models.TextField()
     subtitle = models.TextField()
@@ -17,19 +12,15 @@ class Event(models.Model):
     end_date = models.DateTimeField()
 
     objects = models.Manager()
-    active_only = ActiveEventManager()
 
     class Meta:
         ordering = ["-start_date"]
-        constraints = [ # TODO determine actual business constraints
-            models.CheckConstraint(check=Q(start_date__gt=timezone.now()+timedelta(days=1)),
-                                   name="start_date_gt_one_day"),
-
-            models.CheckConstraint(check=Q(end_date__gt=timezone.now()+timedelta(days=2)),
-                                   name="end_date_gt_two_days"),
-
-            models.CheckConstraint(check=Q(end_date__gt=(F("start_date")+timedelta(days=1))), 
-                                   name="duration_at_least_one_day"),
+        constraints = [
+            models.CheckConstraint(
+                check=Q(end_date__gt=(F("start_date") + timedelta(days=1))),
+                name="duration_at_least_one_day",
+            ),
         ]
+
 
 
