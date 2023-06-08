fb754d75a0482484f124694c9ff0e2d6828d590a
João Righetto <jm_zr@hotmail.com>
2023-06-08 16:46:25 -0300

    feat: Create event model constrains and manager

--- event/models.py
+++ event/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models import Q, F
+
+from .managers import EventManager
 
 
 class Event(models.Model):
@@ -7,5 +10,13 @@ class Event(models.Model):
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
 
+    objects = EventManager()
+
     class Meta:
         ordering = ["-start_date"]
+        constraints = [
+            models.CheckConstraint(
+                check=Q(end_date__gt=F("start_date")),
+                name="end_date_gt_start_date",
+            ),
+        ]
