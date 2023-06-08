52e57a9df5b05ddd281d4073a66bcbd8d61110f0
Joao Marcos <jm_zr@hotmail.com>
2023-06-08 10:42:26 -0300

    feat: create ActiveEventManager and change datetime objects to timezone objects

--- event/models.py
+++ event/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 from django.db.models import Q, F
+from django.utils import timezone
 
-from datetime import datetime, timedelta
+from datetime import timedelta
 
+
+class ActiveEventManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(Q(start_date__gt=timezone.now()))
+        
 
 class Event(models.Model):
     title = models.TextField()
@@ -10,13 +16,16 @@ class Event(models.Model):
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
 
+    objects = models.Manager()
+    active_only = ActiveEventManager()
+
     class Meta:
         ordering = ["-start_date"]
         constraints = [ # TODO determine actual business constraints
-            models.CheckConstraint(check=Q(start_date__gt=datetime.now()+timedelta(days=1)),
+            models.CheckConstraint(check=Q(start_date__gt=timezone.now()+timedelta(days=1)),
                                    name="start_date_gt_one_day"),
 
-            models.CheckConstraint(check=Q(end_date__gt=datetime.now()+timedelta(days=2)),
+            models.CheckConstraint(check=Q(end_date__gt=timezone.now()+timedelta(days=2)),
                                    name="end_date_gt_two_days"),
 
             models.CheckConstraint(check=Q(end_date__gt=(F("start_date")+timedelta(days=1))), 
