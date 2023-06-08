d3a04cd90d7a64e4773b689af02926ccba3f1666
Joao Marcos <jm_zr@hotmail.com>
2023-06-08 13:04:00 -0300

    fix: define get_queryset annotating active events for the EventManager

--- event/managers.py
+++ event/managers.py
@@ -3,11 +3,17 @@ from django.db.models import Manager, BooleanField, ExpressionWrapper, Q
 
 
 class EventManager(Manager):
-    def active(self):
-        qs = self.annotate(
-            is_active=ExpressionWrapper(
-                Q(end_date__gte=timezone.now()),
-                output_field=BooleanField(),
+    def get_queryset(self):
+        return (
+            super()
+            .get_queryset()
+            .annotate(
+                is_active=ExpressionWrapper(
+                    Q(end_date__gte=timezone.now()),
+                    output_field=BooleanField(),
+                )
             )
         )
-        return qs.filter(is_active=True)
+
+    def active(self):
+        return self.filter(is_active=True)
