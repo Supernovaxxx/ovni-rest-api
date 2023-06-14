7b5bb52323bd3b94f2cb1dce0599819d3947ee4c
Joao Marcos <jm_zr@hotmail.com>
2023-06-13 23:34:26 -0300

    feat: create agency model

--- agency/models.py
+++ agency/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Agency(models.Model):
+    title = models.CharField(max_length=50)
+    manager = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "agencies"
