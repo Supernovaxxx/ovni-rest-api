8b1fb24be061b98ae9c76c3f3db178ff890d0b0a
Joao Marcos <jm_zr@hotmail.com>
2023-06-24 12:57:42 -0300

    fix: update owner field with custom user in agency model

--- agency/models.py
+++ agency/models.py
@@ -1,10 +1,15 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.db import models
+
+
+User = get_user_model()
 
 
 class Agency(models.Model):
     title = models.CharField(max_length=50)
-    manager = models.OneToOneField(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, null=True, on_delete=models.SET_NULL) 
 
     class Meta:
-        verbose_name_plural = "agencies"
+        verbose_name_plural = 'agencies'
+
+
