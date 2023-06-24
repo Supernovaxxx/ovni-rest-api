c0cce62189551661ba6669464bfee81a94419ded
Joao Marcos <jm_zr@hotmail.com>
2023-06-24 13:04:30 -0300

    feat: define get_queryset,  get_serializer_class and custom permissions for the agency viewset

--- agency/views.py
+++ agency/views.py
@@ -1,9 +1,24 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets
 from .models import Agency
-from .serializers import AgencySerializer
+from .serializers import ForStaffAgencySerializer, ForOwnerAgencySerializer
+from .permissions import IsOwnerIsStaff
 
 
 class AgencyViewSet(viewsets.ModelViewSet):
-    queryset = Agency.objects.all()
-    serializer_class = AgencySerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [IsOwnerIsStaff,]
+    
+    def get_queryset(self):
+        if self.request.user.is_anonymous:
+            return []
+        
+        if self.request.user.is_superuser:
+            return Agency.objects.all()
+        
+        user = self.request.user
+        
+        return Agency.objects.filter(owner=user)
+    
+    def get_serializer_class(self):
+        if self.request.user.is_staff:
+            return ForStaffAgencySerializer
+        return ForOwnerAgencySerializer
