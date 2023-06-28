302c5c7a1e20f6c14c2375778e5f4a2865597a1f
Joao Marcos <jm_zr@hotmail.com>
2023-06-28 10:01:06 -0300

    fix: remove owner field from agency model

--- agency/models.py
+++ agency/models.py
@@ -1,10 +1,8 @@
-from django.contrib.auth.models import User
 from django.db import models
 
 
 class Agency(models.Model):
     title = models.CharField(max_length=50)
-    manager = models.OneToOneField(User, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name_plural = "agencies"
