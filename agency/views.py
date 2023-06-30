0501bec0db7c221f0247aa7dcc35480cafbd5723
Joao Marcos <jm_zr@hotmail.com>
2023-06-30 12:26:35 -0300

    fix: add read only permissions to safe methods

--- agency/views.py
+++ agency/views.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets, permissions
+
 from .models import Agency
 from .serializers import AgencySerializer
+from .permissions import IsReadOnly
 
 
 class AgencyViewSet(viewsets.ModelViewSet):
     queryset = Agency.objects.all()
     serializer_class = AgencySerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.DjangoObjectPermissions|IsReadOnly]
