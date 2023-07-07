89aa695f1ed95ca1a7d622f0c16a70b4cd16d0e8
Joao Marcos <jm_zr@hotmail.com>
2023-07-07 10:51:30 -0300

    feat: create Tour viewset

--- agency/views.py
+++ agency/views.py
@@ -1,11 +1,17 @@
 from rest_framework import viewsets, permissions
 
-from .models import Agency
-from .serializers import AgencySerializer
+from .models import Agency, Tour
+from .serializers import AgencySerializer, TourSerializer
 from .permissions import IsReadOnly
 
 
 class AgencyViewSet(viewsets.ModelViewSet):
     queryset = Agency.objects.all()
     serializer_class = AgencySerializer
-    permission_classes = [permissions.DjangoObjectPermissions|IsReadOnly]
+    permission_classes = [permissions.DjangoObjectPermissions | IsReadOnly]
+
+
+class TourViewSet(viewsets.ModelViewSet):
+    queryset = Tour.objects.all()
+    serializer_class = TourSerializer
+    permission_classes = [permissions.DjangoObjectPermissions | IsReadOnly]
