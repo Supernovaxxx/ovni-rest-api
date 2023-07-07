37b69b01c419ba82841669896f4d63bab2cefa2b
Joao Marcos <jm_zr@hotmail.com>
2023-07-07 10:50:38 -0300

    feat: create Tour model

--- agency/models.py
+++ agency/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 
+from event.models import Event
+
 
 class Agency(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=50, unique=True)
 
     class Meta:
         verbose_name_plural = "agencies"
+
+
+class Tour(models.Model):
+    agency = models.ForeignKey(Agency, models.DO_NOTHING)
+    event = models.ForeignKey(Event, models.DO_NOTHING)
+
+    class Meta:
+        ordering = ['agency']
