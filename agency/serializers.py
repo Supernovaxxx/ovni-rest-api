839b3b0be539284d327a715ea2b160f126c711be
Joao Marcos <jm_zr@hotmail.com>
2023-07-07 10:50:57 -0300

    feat: create Tour serializer and custom foreign key to the agency model

--- agency/serializers.py
+++ agency/serializers.py
@@ -1,9 +1,25 @@
 from rest_framework import serializers
 
-from .models import Agency
+from guardian.shortcuts import get_objects_for_user
+
+from .models import Agency, Tour
+
+
+class ObjectPermissionFilteredForeignKey(serializers.PrimaryKeyRelatedField):
+    def get_queryset(self):
+        user = self.context['request'].user
+        return get_objects_for_user(user, "change_agency", Agency.objects.all(), accept_global_perms=False)
 
 
 class AgencySerializer(serializers.ModelSerializer):
     class Meta:
         model = Agency
+        fields = '__all__'
+
+
+class TourSerializer(serializers.ModelSerializer):
+    agency = ObjectPermissionFilteredForeignKey()
+
+    class Meta:
+        model = Tour
         fields = '__all__'
