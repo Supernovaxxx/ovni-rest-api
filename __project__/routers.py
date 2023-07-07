7c8bd232ca75cac11b2a425067e347ca3ae8453d
Joao Marcos <jm_zr@hotmail.com>
2023-07-07 10:53:55 -0300

    fix: register Tour viewset in router

--- __project__/routers.py
+++ __project__/routers.py
@@ -1,7 +1,8 @@
 from rest_framework import routers
 from event.views import EventViewSet
-from agency.views import AgencyViewSet
+from agency.views import AgencyViewSet, TourViewSet
 
 router = routers.DefaultRouter()
-router.register('events', EventViewSet, basename='event')
-router.register('agencies', AgencyViewSet, basename='agency')
+router.register('events', EventViewSet, basename='events')
+router.register('agencies', AgencyViewSet, basename='agencies')
+router.register('tours', TourViewSet, basename='tours')
