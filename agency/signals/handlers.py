978076f32ccf9cc231222cb9bee398e108747439
Joao Marcos <jm_zr@hotmail.com>
2023-07-10 11:41:07 -0300

    fix: include relevant permissions

--- agency/signals/handlers.py
+++ agency/signals/handlers.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import Group, Permission
+from django.db.models import Q
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -10,21 +11,25 @@ from ..models import Agency, Tour
 @receiver(post_save, sender=Agency)
 def set_permissions(sender, instance, created, **kwargs):
     """
-    When a new Agency is created, this signal will create a managers group for it and
-    assign the managers permission to edit the Agency.
+    When an Agency is created, this signal will create a managers group for it and
+    assign the managers permission to edit the Agency and create Tours.
     """
-    view = f"view_{instance._meta.model_name}"
-    change = f"change_{instance._meta.model_name}"
 
     if created:
         group = Group.objects.create(name=f'{instance.title} Managers')
 
-        all_perms = Permission.objects.filter(codename__contains="agency")
-        manager_perms = [i for i in all_perms if i.codename in [view, change]]
+        perms = Permission.objects.filter(
+            Q(codename__contains='agency') |
+            Q(codename__contains='tour')
+        )
+
+        manager_perms = [i for i in perms if i.codename in ['view_agency', 'change_agency',
+                                                            'add_tour', 'view_tour',
+                                                            'change_tour', 'delete_tour']]
 
         group.permissions.add(*manager_perms)
         
-        for perm in [view, change]:
+        for perm in ['view_agency', 'change_agency']:
             assign_perm(perm, group, instance)
 
 
@@ -32,18 +37,11 @@ def set_permissions(sender, instance, created, **kwargs):
 def set_permissions(sender, instance, created, **kwargs):
     """
     When a new Tour is created, this signal will retrieve the associated manager group for it and
-    assign the managers permission to edit the Tour.
+    assign the appropriate object level permissions for the Tour.
     """
-    view = f"view_{instance._meta.model_name}"
-    change = f"change_{instance._meta.model_name}"
 
     if created:
         group = Group.objects.get(name=f'{instance.agency.title} Managers')
 
-        all_perms = Permission.objects.filter(codename__contains="tour")
-        manager_perms = [i for i in all_perms if i.codename in [view, change]]
-
-        group.permissions.add(*manager_perms)
-
-        for perm in [view, change]:
+        for perm in ['view_tour', 'change_tour', 'delete_tour']:
             assign_perm(perm, group, instance)
