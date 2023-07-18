48052745d8456ddca7dfce75f8c135dc2c1fb8ca
Joao Marcos <jm_zr@hotmail.com>
2023-07-18 11:15:33 -0300

    fix: implement Place model and serializer

--- geo/models.py
+++ geo/models.py
@@ -1,5 +1,44 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from django_countries.fields import CountryField
 
 
 class Place(models.Model):
-    pass
+    class States(models.TextChoices):
+        AC = "AC", _("Acre")
+        AL = "AL", _("Alagoas")
+        AP = "AP", _("Amapá")
+        AM = "AM", _("Amazonas")
+        BA = "BA", _("Bahia")
+        CE = "CE", _("Ceará")
+        DF = "DF", _("Distrito Federal")
+        ES = "ES", _("Espírito Santo")
+        GO = "GO", _("Goiás")
+        MA = "MA", _("Maranhão")
+        MT = "MT", _("Mato Grosso")
+        MS = "MS", _("Mato Grosso do Sul")
+        MG = "MG", _("Minas Gerais")
+        PA = "PA", _("Pará")
+        PB = "PB", _("Paraíba")
+        PR = "PR", _("Paraná")
+        PE = "PE", _("Pernambuco")
+        PI = "PI", _("Piauí")
+        RJ = "RJ", _("Rio de Janeiro")
+        RN = "RN", _("Rio Grande do Norte")
+        RS = "RS", _("Rio Grande do Sul")
+        RO = "RO", _("Rondônia")
+        RR = "RR", _("Roraima")
+        SC = "SC", _("Santa Catarina")
+        SP = "SP", _("São Paulo")
+        SE = "SE", _("Sergipe")
+        TO = "TO", _("Tocantins")
+
+    title = models.TextField(max_length=50)
+    detail = models.TextField(max_length=255)
+    formatted_address = models.IntegerField()
+    city = models.TextField(max_length=50)
+    state = models.CharField(max_length=2, choices=States.choices)
+    country = CountryField()
+    latitude = models.DecimalField()
+    longitude = models.DecimalField()
