a1ef706481781f40620115a5a2992ebd1e7dfce5
Joao Marcos <jm_zr@hotmail.com>
2023-07-18 11:15:33 -0300

    fix: implement Place model and serializer

--- geo/serializers.py
+++ geo/serializers.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
+from django_countries.serializers import CountryFieldMixin
 
 from .models import Place
 
 
-class PlaceSerializer(serializers.ModelSerializer):
+class PlaceSerializer(CountryFieldMixin, serializers.ModelSerializer):
     class Meta:
         model = Place
         fields = "__all__"
