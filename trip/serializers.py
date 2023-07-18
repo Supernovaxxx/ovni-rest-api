a2fb65e7f71f69f3a8f387cab167e20f82a1fadf
Joao Marcos <jm_zr@hotmail.com>
2023-07-18 00:03:21 -0300

    fix: create nested relationship between Trip and Waypoint inside TripSerializer

--- trip/serializers.py
+++ trip/serializers.py
@@ -3,13 +3,21 @@ from rest_framework import serializers
 from .models import Trip, Waypoint
 
 
-class TripSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Trip
-        fields = "__all__"
-
-
 class WaypointSerializer(serializers.ModelSerializer):
     class Meta:
         model = Waypoint
         fields = "__all__"
+
+
+class TripSerializer(serializers.ModelSerializer):
+    waypoints = WaypointSerializer(many=True)
+
+    class Meta:
+        model = Trip
+        fields = "__all__"
+
+    def create(self, validated_data):
+        waypoints = validated_data.pop("waypoints")
+        trip = Trip.objects.create(**validated_data)
+        Waypoint.objects.bulk_create(waypoints)
+        return trip
