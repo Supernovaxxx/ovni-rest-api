e085c55d1e65fc0306d2279daa465e2c332dadf3
Joao Marcos <jm_zr@hotmail.com>
2023-07-20 23:24:38 -0300

    fix: remove unnecessary code and fix field options

--- geo/models.py
+++ geo/models.py
@@ -6,39 +6,39 @@ from django_countries.fields import CountryField
 
 class Place(models.Model):
     class States(models.TextChoices):
-        AC = "AC", _("Acre")
-        AL = "AL", _("Alagoas")
-        AP = "AP", _("Amapá")
-        AM = "AM", _("Amazonas")
-        BA = "BA", _("Bahia")
-        CE = "CE", _("Ceará")
-        DF = "DF", _("Distrito Federal")
-        ES = "ES", _("Espírito Santo")
-        GO = "GO", _("Goiás")
-        MA = "MA", _("Maranhão")
-        MT = "MT", _("Mato Grosso")
-        MS = "MS", _("Mato Grosso do Sul")
-        MG = "MG", _("Minas Gerais")
-        PA = "PA", _("Pará")
-        PB = "PB", _("Paraíba")
-        PR = "PR", _("Paraná")
-        PE = "PE", _("Pernambuco")
-        PI = "PI", _("Piauí")
-        RJ = "RJ", _("Rio de Janeiro")
-        RN = "RN", _("Rio Grande do Norte")
-        RS = "RS", _("Rio Grande do Sul")
-        RO = "RO", _("Rondônia")
-        RR = "RR", _("Roraima")
-        SC = "SC", _("Santa Catarina")
-        SP = "SP", _("São Paulo")
-        SE = "SE", _("Sergipe")
-        TO = "TO", _("Tocantins")
+        AC = "AC"
+        AL = "AL"
+        AP = "AP"
+        AM = "AM"
+        BA = "BA"
+        CE = "CE"
+        DF = "DF"
+        ES = "ES"
+        GO = "GO"
+        MA = "MA"
+        MT = "MT"
+        MS = "MS"
+        MG = "MG"
+        PA = "PA"
+        PB = "PB"
+        PR = "PR"
+        PE = "PE"
+        PI = "PI"
+        RJ = "RJ"
+        RN = "RN"
+        RS = "RS"
+        RO = "RO"
+        RR = "RR"
+        SC = "SC"
+        SP = "SP"
+        SE = "SE"
+        TO = "TO"
 
     title = models.TextField(max_length=50)
     detail = models.TextField(max_length=255)
-    formatted_address = models.IntegerField()
+    formatted_address = models.TextField(max_length=50)
     city = models.TextField(max_length=50)
     state = models.CharField(max_length=2, choices=States.choices)
     country = CountryField()
-    latitude = models.DecimalField()
-    longitude = models.DecimalField()
+    latitude = models.DecimalField(decimal_places=2, max_digits=50)
+    longitude = models.DecimalField(decimal_places=2, max_digits=50)
