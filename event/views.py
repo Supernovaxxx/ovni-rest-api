b4bf896eb02def5109ee3c5591773ad29c1bbdfd
Joao Marcos <jm_zr@hotmail.com>
2023-07-21 11:19:54 -0300

    fix: add proper permission classes to EventViewSet

--- event/views.py
+++ event/views.py
@@ -1,8 +1,11 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
+
 from .models import Event
 from .serializers import EventSerializer
+from agency.permissions import IsReadOnly
 
 
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+    permission_classes = [permissions.IsAdminUser | IsReadOnly]
