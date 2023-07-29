f512163f09526c1239f66fd4f31e303adcf80b36
Joao Marcos <jm_zr@hotmail.com>
2023-07-29 01:28:44 -0300

    fix: make model fields nullable and blankable

--- geo/models.py
+++ geo/models.py
@@ -1,44 +1,13 @@
 from django.db import models
-from django.utils.translation import gettext_lazy as _
-
-from django_countries.fields import CountryField
 
 
 class Place(models.Model):
-    class States(models.TextChoices):
-        AC = "AC"
-        AL = "AL"
-        AP = "AP"
-        AM = "AM"
-        BA = "BA"
-        CE = "CE"
-        DF = "DF"
-        ES = "ES"
-        GO = "GO"
-        MA = "MA"
-        MT = "MT"
-        MS = "MS"
-        MG = "MG"
-        PA = "PA"
-        PB = "PB"
-        PR = "PR"
-        PE = "PE"
-        PI = "PI"
-        RJ = "RJ"
-        RN = "RN"
-        RS = "RS"
-        RO = "RO"
-        RR = "RR"
-        SC = "SC"
-        SP = "SP"
-        SE = "SE"
-        TO = "TO"
-
-    title = models.TextField(max_length=50)
-    detail = models.TextField(max_length=255)
-    formatted_address = models.TextField(max_length=50)
-    city = models.TextField(max_length=50)
-    state = models.CharField(max_length=2, choices=States.choices)
-    country = CountryField()
-    latitude = models.DecimalField(decimal_places=2, max_digits=50)
-    longitude = models.DecimalField(decimal_places=2, max_digits=50)
+    place_id = models.CharField(max_length=255, primary_key=True)
+    title = models.TextField(max_length=50, null=True, blank=True)
+    detail = models.TextField(max_length=255, null=True, blank=True)
+    formatted_address = models.TextField(max_length=255, null=True, blank=True)
+    city = models.TextField(max_length=50, null=True, blank=True)
+    state = models.TextField(max_length=50, null=True, blank=True)
+    country = models.TextField(max_length=50, null=True, blank=True)
+    latitude = models.DecimalField(decimal_places=8, max_digits=20, null=True, blank=True)
+    longitude = models.DecimalField(decimal_places=8, max_digits=20, null=True, blank=True)
