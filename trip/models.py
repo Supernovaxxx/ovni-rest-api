77155e4a74a7a8fca9394be52d1b9505f2c37f9c
Joao Marcos <jm_zr@hotmail.com>
2023-07-29 01:28:44 -0300

    fix: make model fields nullable and blankable

--- trip/models.py
+++ trip/models.py
@@ -7,9 +7,9 @@ from geo.models import Place
 
 class Trip(models.Model):
     tour = models.ForeignKey(Tour, models.PROTECT)
-    slug = models.SlugField()
-    departure = models.DateTimeField()
-    capacity = models.IntegerField()
+    slug = models.SlugField(null=True, blank=True)
+    departure = models.DateTimeField(null=True, blank=True)
+    capacity = models.IntegerField(null=True, blank=True)
 
 
 class Waypoint(models.Model):
@@ -18,8 +18,8 @@ class Waypoint(models.Model):
         STOP = "Stop"
         ATTRACTIVE = "Attractive"
 
-    trip = models.ForeignKey(Trip, models.CASCADE, related_name="route", null=True)
-    place = models.ForeignKey(Place, models.PROTECT)
+    trip = models.ForeignKey(Trip, models.CASCADE, related_name="route", null=True, blank=True)
+    place = models.ForeignKey(Place, models.PROTECT, related_name="waypoints", null=True, blank=True)
     order = models.IntegerField(null=True)
     type = models.CharField(max_length=50, choices=Types.choices)
 
