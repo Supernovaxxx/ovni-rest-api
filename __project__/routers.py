a64d805dd968c675769bf9238f21a41e223ec938
Joao Marcos <jm_zr@hotmail.com>
2023-07-29 01:24:34 -0300

    fix: delete place viewset

--- __project__/routers.py
+++ __project__/routers.py
@@ -2,11 +2,9 @@ from rest_framework import routers
 from event.views import EventViewSet
 from agency.views import AgencyViewSet, TourViewSet
 from trip.views import TripViewSet
-from geo.views import PlaceViewSet
 
 router = routers.DefaultRouter()
 router.register('events', EventViewSet, basename='events')
 router.register('agencies', AgencyViewSet, basename='agencies')
 router.register('tours', TourViewSet, basename='tours')
 router.register('trips', TripViewSet, basename='trips')
-router.register('places', PlaceViewSet, basename='places')
