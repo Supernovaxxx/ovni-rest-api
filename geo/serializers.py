292871ee9e063ca36b8553150def7f6632f57ce3
Joao Marcos <jm_zr@hotmail.com>
2023-07-30 12:45:28 -0300

    refactor: simplify PlaceSerializer update method and improve meaning to variable names

--- geo/serializers.py
+++ geo/serializers.py
@@ -11,19 +11,21 @@ from .models import Place
 GEOCODE_BASE_URL = "https://maps.googleapis.com/maps/api/geocode/json"
 
 
-def _get_place_data_from_google_api(place_id):
+def _get_updated_fields_from_google_api(place_id):
     place_data = requests.get(GEOCODE_BASE_URL,
                               params={"place_id": place_id,
                                       "key": GOOGLE_MAPS_API_KEY},
                               )
 
     place_data = json.loads(place_data.text)["results"][0]
+
     formatted_address = place_data["formatted_address"]
     latitude = place_data["geometry"]["location"]["lat"]
     longitude = place_data["geometry"]["location"]["lng"]
     city = None
     state = None
     country = None
+
     for item in place_data["address_components"]:
         if city is None and "administrative_area_level_2" in item["types"]:
             city = item["short_name"]
@@ -43,7 +45,7 @@ class PlaceSerializer(UniqueFieldsMixin, serializers.ModelSerializer):
     def create(self, validated_data):
         place_id = validated_data["place_id"]
 
-        formatted_address, latitude, longitude, city, state, country = _get_place_data_from_google_api(place_id)
+        formatted_address, latitude, longitude, city, state, country = _get_updated_fields_from_google_api(place_id)
 
         return Place.objects.create(place_id=place_id,
                                     formatted_address=formatted_address,
@@ -59,14 +61,10 @@ class PlaceSerializer(UniqueFieldsMixin, serializers.ModelSerializer):
         else:
             place_id = validated_data["place_id"]
 
-            formatted_address, latitude, longitude, city, state, country = _get_place_data_from_google_api(place_id)
+            updated_data = _get_updated_fields_from_google_api(place_id)
 
-            instance.formatted_address = formatted_address
-            instance.latitude = latitude
-            instance.longitude = longitude
-            instance.city = city
-            instance.state = state
-            instance.country = country
+            for field, data in self.fields, updated_data:
+                instance.field = data
 
             instance.save()
 
