1edb9e095c6742c6b4e4498784d0ea2f5fb6cb37
Joao Marcos <jm_zr@hotmail.com>
2023-07-31 20:48:13 -0300

    refactor: move the Place creation logic from the PlaceSerializer to the Place model by overriding its save() method

--- geo/models.py
+++ geo/models.py
@@ -1,4 +1,12 @@
+import requests
+import json
+
 from django.db import models
+
+from __project__.settings import GOOGLE_MAPS_API_KEY
+
+
+GEOCODE_BASE_URL = "https://maps.googleapis.com/maps/api/geocode/json"
 
 
 class Place(models.Model):
@@ -11,3 +19,27 @@ class Place(models.Model):
     country = models.TextField(max_length=50, null=True, blank=True)
     latitude = models.DecimalField(decimal_places=8, max_digits=20, null=True, blank=True)
     longitude = models.DecimalField(decimal_places=8, max_digits=20, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        place_data = requests.get(GEOCODE_BASE_URL,
+                                  params={"place_id": self.place_id,
+                                          "key": GOOGLE_MAPS_API_KEY},
+                                  )
+
+        place_data = json.loads(place_data.text)["results"][0]
+
+        self.formatted_address = place_data["formatted_address"]
+        self.latitude = place_data["geometry"]["location"]["lat"]
+        self.longitude = place_data["geometry"]["location"]["lng"]
+
+        for item in place_data["address_components"]:
+            if "administrative_area_level_2" in item["types"]:
+                self.city = item["short_name"]
+
+            if "administrative_area_level_1" in item["types"]:
+                self.state = item["short_name"]
+
+            if "country" in item["types"]:
+                self.country = item["short_name"]
+
+        super(Place, self).save(*args, **kwargs)
--- geo/serializers.py
+++ geo/serializers.py
@@ -1,71 +1,10 @@
-import requests
-import json
-
 from drf_writable_nested import UniqueFieldsMixin
 from rest_framework import serializers
 
-from __project__.settings import GOOGLE_MAPS_API_KEY
 from .models import Place
-
-
-GEOCODE_BASE_URL = "https://maps.googleapis.com/maps/api/geocode/json"
-
-
-def _get_updated_fields_from_google_api(place_id):
-    place_data = requests.get(GEOCODE_BASE_URL,
-                              params={"place_id": place_id,
-                                      "key": GOOGLE_MAPS_API_KEY},
-                              )
-
-    place_data = json.loads(place_data.text)["results"][0]
-
-    formatted_address = place_data["formatted_address"]
-    latitude = place_data["geometry"]["location"]["lat"]
-    longitude = place_data["geometry"]["location"]["lng"]
-    city = None
-    state = None
-    country = None
-
-    for item in place_data["address_components"]:
-        if city is None and "administrative_area_level_2" in item["types"]:
-            city = item["short_name"]
-        if state is None and "administrative_area_level_1" in item["types"]:
-            state = item["short_name"]
-        if country is None and "country" in item["types"]:
-            country = item["short_name"]
-
-    return formatted_address, latitude, longitude, city, state, country
 
 
 class PlaceSerializer(UniqueFieldsMixin, serializers.ModelSerializer):
     class Meta:
         model = Place
         fields = "__all__"
-
-    def create(self, validated_data):
-        place_id = validated_data["place_id"]
-
-        formatted_address, latitude, longitude, city, state, country = _get_updated_fields_from_google_api(place_id)
-
-        return Place.objects.create(place_id=place_id,
-                                    formatted_address=formatted_address,
-                                    city=city,
-                                    state=state,
-                                    country=country,
-                                    latitude=latitude,
-                                    longitude=longitude)
-
-    def update(self, instance, validated_data):
-        if validated_data["place_id"] == instance.place_id:
-            return instance
-        else:
-            place_id = validated_data["place_id"]
-
-            updated_data = _get_updated_fields_from_google_api(place_id)
-
-            for field, data in self.fields, updated_data:
-                instance.field = data
-
-            instance.save()
-
-            return instance
