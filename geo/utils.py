bb905bf9dd7f924df176f514e8fe684ecd50bc88
Joao Marcos <jm_zr@hotmail.com>
2023-08-01 20:55:27 -0300

    fix: isolate google maps API consumption inside utils.py file

---
+++ geo/utils.py
@@ -0,0 +1,35 @@
+import requests
+import json
+
+from __project__.settings import GOOGLE_MAPS_API_KEY
+
+
+def consume_place_data_from_google_api(**kwargs):
+    geocode_base_url = "https://maps.googleapis.com/maps/api/geocode/json"
+
+    response = requests.get(geocode_base_url,
+                            params={"place_id": kwargs["place_id"],
+                                    "key": GOOGLE_MAPS_API_KEY},
+                            )
+
+    response.raise_for_status()
+
+    ret = {}
+
+    place_data = json.loads(response.text)["results"][0]
+
+    ret["formatted_address"] = place_data["formatted_address"]
+    ret["latitude"] = place_data["geometry"]["location"]["lat"]
+    ret["longitude"] = place_data["geometry"]["location"]["lng"]
+
+    for item in place_data["address_components"]:
+        if "administrative_area_level_2" in item["types"]:
+            ret["city"] = item["short_name"]
+
+        if "administrative_area_level_1" in item["types"]:
+            ret["state"] = item["short_name"]
+
+        if "country" in item["types"]:
+            ret["country"] = item["short_name"]
+
+    return ret
