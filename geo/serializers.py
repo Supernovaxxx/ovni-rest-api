65dd079f1e2e75f860fa37bf8ff0b194762dd8db
Joao Marcos <jm_zr@hotmail.com>
2023-07-31 21:03:57 -0300

    docs: add comments for improved code documentation and readability.

--- geo/serializers.py
+++ geo/serializers.py
@@ -1,5 +1,8 @@
-from drf_writable_nested import UniqueFieldsMixin
 from rest_framework import serializers
+
+# Without UniqueFieldsMixin, trying to create a Trip with an already existing Place will result in
+# "400 Bad request" error stating there's already a Place with the given place_id
+from drf_writable_nested import UniqueFieldsMixin
 
 from .models import Place
 
