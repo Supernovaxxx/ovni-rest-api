0988f46138a881eec5854c6c5f01a4e8def8aec2
Joao Marcos <jm_zr@hotmail.com>
2023-08-02 22:57:10 -0300

    fix: adjust position of DRF_STANDARDIZED_ERRORS option closer to REST_FRAMEWORK's options

--- __project__/settings.py
+++ __project__/settings.py
@@ -160,6 +160,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+    'EXCEPTION_HANDLER': 'drf_standardized_errors.handler.exception_handler'
 }
 
 
