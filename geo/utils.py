87e4d6a91f38a52436f537484ee28e4f9c79ec80
Joao Marcos <jm_zr@hotmail.com>
2023-08-03 14:24:14 -0300

    fix: optimize error handling for invalid place_id input

--- geo/utils.py
+++ geo/utils.py
@@ -6,17 +6,23 @@ from __project__.settings import GOOGLE_MAPS_API_KEY
 
 def consume_place_data_from_google_api(**kwargs):
     geocode_base_url = "https://maps.googleapis.com/maps/api/geocode/json"
+    try:
+        response = requests.get(geocode_base_url,
+                                params={"place_id": kwargs["place_id"],
+                                        "key": GOOGLE_MAPS_API_KEY},
+                                )
 
-    response = requests.get(geocode_base_url,
-                            params={"place_id": kwargs["place_id"],
-                                    "key": GOOGLE_MAPS_API_KEY},
-                            )
+        response.raise_for_status()
 
-    response.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        raise ValueError(e)
 
     ret = {}
 
-    place_data = json.loads(response.text)["results"][0]
+    try:
+        place_data = json.loads(response.text)["results"][0]
+    except IndexError:
+        raise ValueError("The response for this 'place_id' has returned no results.")
 
     ret["formatted_address"] = place_data["formatted_address"]
 
