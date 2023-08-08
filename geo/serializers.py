fb8c38fe00403aac1987d1e2c7a1f7cdf1317de6
Arthur Henrique <Arthur.Henrique.Della.Fraga@gmail.com>
2023-08-08 12:40:48 -0300

    refactor: Use PlaceManager to deal with google maps creation

--- geo/serializers.py
+++ geo/serializers.py
@@ -1,15 +1,8 @@
-from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import serializers
-
-# Without UniqueFieldsMixin, trying to create a Trip with an already existing Place will result in
-# "400 Bad request" error stating there's already a Place with the given place_id
-from drf_writable_nested import UniqueFieldsMixin
-
 from .models import Place
-from .utils import consume_place_data_from_google_api
 
 
-class PlaceSerializer(UniqueFieldsMixin, serializers.ModelSerializer):
+class PlaceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Place
         fields = "__all__"
@@ -18,7 +11,5 @@ class PlaceSerializer(UniqueFieldsMixin, serializers.ModelSerializer):
         return place_id
 
     def to_internal_value(self, place_id):
-        try:
-            return Place.objects.get(pk=place_id)
-        except ObjectDoesNotExist:
-            return self.create(consume_place_data_from_google_api(place_id))
+        place, _ = Place.objects.get_or_create_from_maps_api(place_id)
+        return place
