67f98cfca745114ee37e6ccf8707995aebfc1e75
Arthur Henrique <Arthur.Henrique.Della.Fraga@gmail.com>
2023-08-09 19:38:32 -0300

    refactor: Readability improvements and documentation on geo managers and serializers

--- geo/utils.py
+++ geo/utils.py
@@ -1,35 +1,22 @@
 import requests
-import json
 
 from __project__.settings import GOOGLE_MAPS_API_KEY
 
 
-def consume_place_data_from_google_api(place_id):
-    geocode_base_url = "https://maps.googleapis.com/maps/api/geocode/json"
-
-    try:
-        response = requests.get(geocode_base_url,
-                                params={"place_id": place_id,
-                                        "key": GOOGLE_MAPS_API_KEY},
-                                )
-
-        response.raise_for_status()
-    except requests.exceptions.HTTPError as e:
-        raise ValueError(e)
-
-    try:
-        place_data = json.loads(response.text)["results"][0]
-    except IndexError:
-        raise ValueError("The response for this 'place_id' has returned no results.")
+def get_place_data_from_geocode_api(place_id):
+    geocode_data = _get_geocode_data(
+        place_id=place_id,
+        key=GOOGLE_MAPS_API_KEY,
+    )
 
     ret = {
         "place_id": place_id,
-        "formatted_address": place_data["formatted_address"],
-        "latitude": place_data["geometry"]["location"]["lat"],
-        "longitude": place_data["geometry"]["location"]["lng"],
+        "formatted_address": geocode_data["formatted_address"],
+        "latitude": geocode_data["geometry"]["location"]["lat"],
+        "longitude": geocode_data["geometry"]["location"]["lng"],
     }
 
-    for item in place_data["address_components"]:
+    for item in geocode_data["address_components"]:
         if "administrative_area_level_2" in item["types"]:
             ret["city"] = item["short_name"]
 
@@ -40,3 +27,20 @@ def consume_place_data_from_google_api(place_id):
             ret["country"] = item["short_name"]
 
     return ret
+
+
+def _get_geocode_data(**params):
+    """Interface with Google Maps Geocode API"""
+    geocode_api_url = "https://maps.googleapis.com/maps/api/geocode/json"
+
+    try:
+        response = requests.get(geocode_api_url, **params)
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        raise ValueError(e)
+
+    result = response.json()["results"]
+    if not result:
+        raise ValueError(f"The provided 'place_id' has no correspondence on Google Maps Geocode API")
+
+    return result[0]
