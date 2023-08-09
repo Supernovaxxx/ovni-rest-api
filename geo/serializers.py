1ec737c45cd7b093f06e5edd6849ce0441e88eb8
Arthur Henrique <Arthur.Henrique.Della.Fraga@gmail.com>
2023-08-09 13:04:31 -0300

    feat: Fine tunning place-flatten waypoint serialization

--- geo/serializers.py
+++ geo/serializers.py
@@ -7,9 +7,6 @@ class PlaceSerializer(serializers.ModelSerializer):
         model = Place
         fields = "__all__"
 
-    def to_representation(self, place_id):
-        return place_id
-
     def to_internal_value(self, place_id):
         place, _ = Place.objects.get_or_create_from_maps_api(place_id)
         return place
