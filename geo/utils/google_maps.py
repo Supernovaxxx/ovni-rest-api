9e606c2269a531e8a164c7dd08c61f9f45b506cf
Arthur Henrique <Arthur.Henrique.Della.Fraga@gmail.com>
2023-08-16 22:57:31 -0300

    refactor: Improve google maps api client compatibility

--- geo/utils/google_maps.py
+++ geo/utils/google_maps.py
@@ -1,4 +1,32 @@
 import googlemaps
-from django.conf import settings
+from googlemaps.exceptions import ApiError, HTTPError
+from geo.env import env, ImproperlyConfigured
 
-GOOGLE_MAPS_API_CLIENT = googlemaps.Client(key=settings.GOOGLE_MAPS_API_KEY)
+
+class GoogleMapsApiClient:
+    def __init__(self):
+        if GOOGLE_MAPS_API_KEY := env("GOOGLE_MAPS_API_KEY"):
+            self._client = googlemaps.Client(key=GOOGLE_MAPS_API_KEY)
+
+    @property
+    def client(self, **params):
+        if not hasattr(self, "_client"):
+            raise ImproperlyConfigured(
+                "Missing GOOGLE_MAPS_API_KEY environment variable."
+            )
+        return self._client
+
+    def geocode(self, **params):
+        """Interface with Google Maps Geocode API"""
+        try:
+            result = self.client.geocode(**params)
+            return result[0]
+        except (ApiError, HTTPError) as e:
+            raise e
+        except Exception as e:
+            raise ValueError(
+                "No correspondence found on Google Maps Geocode API"
+            ) from e
+
+
+GOOGLE_MAPS_API_CLIENT = GoogleMapsApiClient()
