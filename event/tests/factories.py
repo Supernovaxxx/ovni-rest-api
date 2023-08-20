f54f968f54d43566e06e17374a010ef8ba1e6b7d
Joao Marcos <jm_zr@hotmail.com>
2023-08-19 23:18:08 -0300

    refactor: delete UserFactory, create InactiveEventFactory and improve factories readability

--- event/tests/factories.py
+++ event/tests/factories.py
@@ -1,35 +1,36 @@
-import factory
+import datetime as dt
 
+import factory
 from factory import fuzzy
 from faker import Faker
-from datetime import datetime, timedelta, timezone
-
-from django.contrib.auth import get_user_model
 
 from event.models import Event
 
-User = get_user_model()
 fake = Faker()
-now = datetime.now(timezone.utc)
+
+
+def now():
+    return dt.datetime.now(dt.UTC)
+
+
+def random_past_date(weeks_range=12):
+    return fuzzy.FuzzyDateTime(now() - dt.timedelta(weeks=weeks_range)).fuzz()
+
+
+def random_future_date():
+    return fuzzy.FuzzyDateTime(now(), now() + dt.timedelta(days=7)).fuzz()
 
 
 class EventFactory(factory.django.DjangoModelFactory):
-    """
-    Creates an active event by default.
-    """
     class Meta:
         model = Event
 
     title = factory.Sequence(lambda n: f"event_{n:04}")
-    subtitle = fake.sentence(nb_words=10, variable_nb_words=True)
-    start_date = fuzzy.FuzzyDateTime(datetime(2023, 1, 1, tzinfo=timezone.utc))
-    end_date = fuzzy.FuzzyDateTime(now, now + timedelta(days=7))
+    subtitle = fake.sentence(nb_words=5, variable_nb_words=True)
+    start_date = random_past_date()
+    end_date = random_future_date()
 
 
-class UserFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = User
-
-    username = factory.Sequence(lambda n: f"user_{n:04}")
-    email = factory.LazyAttribute(lambda user: f"{user.username}@test.com")
-    password = factory.django.Password("pw")
+class InactiveEventFactory(EventFactory):
+    start_date = random_past_date()
+    end_date = start_date + dt.timedelta(minutes=1)
