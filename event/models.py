39cc3108f8e90b90d05a429e170ab6f2af9fb76d
Joao Marcos <jm_zr@hotmail.com>
2023-08-30 01:30:37 -0300

    fix: change title and subtitle fields to CharField and make subtitle field nullable

--- event/models.py
+++ event/models.py
@@ -6,8 +6,8 @@ from .managers import EventManager
 
 
 class Event(models.Model):
-    title = models.TextField(validators=[MinLengthValidator(3)])
-    subtitle = models.TextField()
+    title = models.CharField(max_length=255, validators=[MinLengthValidator(3)])
+    subtitle = models.CharField(max_length=255, null=True)
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
 
