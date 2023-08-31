b7558f63e347991329012ab3efff02bc4f324ca6
Joao Marcos <jm_zr@hotmail.com>
2023-08-30 23:22:51 -0300

    refactor: start using factory_boy as data generation engine

--- event/tests/test_models_cases.py
+++ event/tests/test_models_cases.py
@@ -1,70 +1,33 @@
-import datetime as dt
+import factory
+
+from .factories import EventFactory
+
 from pytest_cases import case, parametrize
-from faker import Faker
-
-
-fake = Faker()
-
-
-def random_title():
-    return fake.sentence(nb_words=2, variable_nb_words=True)
-
-
-def random_subtitle():
-    return fake.sentence(nb_words=5, variable_nb_words=True)
-
-
-def random_past_date():
-    return fake.past_datetime(tzinfo=dt.UTC)
-
-
-def random_future_date():
-    return fake.future_datetime(tzinfo=dt.UTC)
-
-
-def create_event_data_from_key_values(blank_key=None, null_key=None):
-    key_values = [
-        ("title", random_title()),
-        ("subtitle", random_subtitle()),
-        ("start_date", random_past_date()),
-        ("end_date", random_future_date()),
-    ]
-    if blank_key:
-        data = {k: (v if k != blank_key else "") for (k, v) in key_values}
-        if blank_key != "subtitle":
-            data["validity"] = False
-        else:
-            data["validity"] = True
-
-    elif null_key:
-        data = {k: (v if k != null_key else None) for (k, v) in key_values}
-        data["validity"] = False
-
-    else:
-        data = {k: v for (k, v) in key_values}
-        data["validity"] = True
-
-    return data
 
 
 class EventData:
     @case(tags="valid_data")
     def case_valid_data(self):
-        return create_event_data_from_key_values()
+        event_data = factory.build(dict, FACTORY_CLASS=EventFactory)
+        event_data["validity"] = True
+        return event_data
 
     @parametrize(blank_key=["title", "subtitle", "start_date", "end_date"])
     def case_blank_field_in_data(self, blank_key: str):
-        return create_event_data_from_key_values(blank_key=blank_key)
+        data = factory.build(dict, FACTORY_CLASS=EventFactory)
+        data[blank_key] = ""
+        data["validity"] = True if blank_key == "subtitle" else False
+        return data
 
     @parametrize(null_key=["title", "subtitle", "start_date", "end_date"])
     def case_null_field_in_data(self, null_key: str):
-        return create_event_data_from_key_values(null_key=null_key)
+        data = factory.build(dict, FACTORY_CLASS=EventFactory)
+        data[null_key] = None
+        data["validity"] = True if null_key == "subtitle" else False
+        return data
 
-    def case_invalid_dates_in_data(self):
-        return {
-            "title": random_title(),
-            "subtitle": random_subtitle(),
-            "start_date": random_future_date(),
-            "end_date": random_past_date(),
-            "validity": False,
-        }
+    @case(tags="invalid_data")
+    def case_invalid_data(self):
+        event_data = factory.build(dict, FACTORY_CLASS=EventFactory, invalid=True)
+        event_data["validity"] = False
+        return event_data
