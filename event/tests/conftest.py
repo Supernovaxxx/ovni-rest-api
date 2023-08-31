056152fef88228c093ac95681f278d7959877b0c
Joao Marcos <jm_zr@hotmail.com>
2023-08-31 13:35:56 -0300

    refactor: create a more similar way to call the populate_db_with_events as if it was calling the custom command generate_event_data

--- event/tests/conftest.py
+++ event/tests/conftest.py
@@ -1,12 +1,11 @@
 import pytest
 
-from event.management.commands.generate_event_data import DEFAULT_QUANTITY
 from django.core.management import call_command
 
 
 @pytest.fixture
 @pytest.mark.django_db
 def populate_db_with_events():
-    def _populate(nb_events=DEFAULT_QUANTITY, nb_upcoming=0, nb_others=0):
-        call_command("generate_event_data", nb_events, upcoming=nb_upcoming, past=nb_others)
+    def _populate(*args, **kwargs):
+        call_command("generate_event_data", *args, **kwargs)
     return _populate
