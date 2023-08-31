99697f77f9ebf2a42272e6d61513956f7f60f73f
Joao Marcos <jm_zr@hotmail.com>
2023-08-30 23:06:29 -0300

    fix: delete unnecessary user factory

--- conftest.py
+++ conftest.py
@@ -1,6 +1,4 @@
 from pytest_factoryboy import register
 from event.tests.factories import EventFactory
-from management.tests.factories import UserFactory
 
 register(EventFactory)
-register(UserFactory)
