ba95fcdefb52130249df025d5f61d710a6983c2e
Joao Marcos <jm_zr@hotmail.com>
2023-09-11 15:07:19 -0300

    feat: create custom ModelAdmin for every model centralizing most actions inside the TourAdmin

--- trip/admin.py
+++ trip/admin.py
@@ -2,24 +2,15 @@ from django.contrib import admin
 
 from guardian.admin import GuardedModelAdmin
 
-from .models import Trip, Waypoint
+from trip.models import Waypoint, Trip
+from agency import sites
 
 
-@admin.register(Trip)
-class TripAdmin(GuardedModelAdmin):
-    list_display = ["tour", "slug", "departure", "capacity"]
-    list_filter = ["departure", "capacity"]
-    search_fields = ["tour__agency__title", "tour__event__title"]
+class WaypointAdminInline(admin.StackedInline):
+    model = Waypoint
+    extra = 0
 
 
-@admin.register(Waypoint)
-class WaypointAdmin(admin.ModelAdmin):
-    list_display = ["title", "type", "trip", "place", "order"]
-    list_filter = ["type", "order"]
-    search_fields = [
-        "trip__tour__agency__title",
-        "trip__tour__event__title",
-        "place__city",
-        "place__state",
-        "place__country",
-    ]
+class TripAdminInline(admin.StackedInline):
+    model = Trip
+    extra = 0
