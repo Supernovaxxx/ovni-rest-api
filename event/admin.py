9eed29c57d4b0959c38e075e82c7a3fc8289fa0f
Joao Marcos <jm_zr@hotmail.com>
2023-09-11 15:07:19 -0300

    feat: create custom ModelAdmin for every model centralizing most actions inside the TourAdmin

--- event/admin.py
+++ event/admin.py
@@ -1,10 +1,45 @@
+from datetime import datetime, UTC
+
+
 from django.contrib import admin
-from .models import Event
+from django.utils.html import format_html
+from django.urls import reverse
+
+from agency.sites import agency_admin_site
+from event.models import Event
 
 
-@admin.register(Event)
+class IsUpcomingFilter(admin.SimpleListFilter):
+    title = 'upcoming'
+    parameter_name = 'is_upcoming'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('Yes', 'Yes'),
+            ('No', 'No'),
+        )
+
+    def queryset(self, request, queryset):
+        value = self.value()
+        if value == 'Yes':
+            return queryset.filter(start_date__gt=datetime.now(UTC))
+        elif value == 'No':
+            return queryset.exclude(start_date__gt=datetime.now(UTC))
+        return queryset
+
+
+@admin.register(Event, site=agency_admin_site)
 class EventAdmin(admin.ModelAdmin):
-    list_display = ["title", "start_date", "end_date"]
-    list_filter = ["start_date", "end_date"]
+    list_display = ["title", "start_date", "end_date", "is_upcoming", "create_tour"]
+    list_filter = ["start_date", "end_date", IsUpcomingFilter]
     fields = [("title", "subtitle"), ("start_date", "end_date")]
     search_fields = ["title"]
+
+    def is_upcoming(self, obj):
+        return obj.start_date > datetime.now(UTC)
+
+    @admin.display()
+    def create_tour(self, obj):
+        url = reverse("agency_admin:agency_tour_add")
+        url += f"?event={obj.id}"
+        return format_html("<a href='{}'>Create Tour</a>", url)
