a9ae453279e909b8519fd469943a8f661d0cae81
Arthur Henrique <Arthur.Henrique.Della.Fraga@gmail.com>
2023-10-05 14:01:43 -0300

    refactor: Opt for extend SimpleAdminConfig on agency apps

--- agency/__init__.py
+++ agency/__init__.py
@@ -1,7 +1,1 @@
-from django.utils.module_loading import autodiscover_modules
 
-from agency import sites
-
-
-def autodiscover():
-    autodiscover_modules("admin", register_to=sites.agency_admin_site)
--- agency/apps.py
+++ agency/apps.py
@@ -1,11 +1,15 @@
 from django.contrib.admin import apps
+from django.utils.module_loading import autodiscover_modules
+
+from agency import sites
 
 
-class AgencyConfig(apps.AdminConfig):
+class AgencyConfig(apps.SimpleAdminConfig):
     default_site = "agency.sites.AgencyAdminSite"
     default_auto_field = "django.db.models.BigAutoField"
     name = "agency"
 
     def ready(self):
         super().ready()
+        autodiscover_modules("admin", register_to=sites.agency_admin_site)
         import agency.signals.handlers
