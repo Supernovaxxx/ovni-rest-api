e65d09c1400a590760c90dc53ca167920fa17414
Arthur Henrique <Arthur.Henrique.Della.Fraga@gmail.com>
2023-10-05 19:04:48 -0300

    refactor: Event Admin

--- event/admin.py
+++ event/admin.py
@@ -1,7 +1,8 @@
-from datetime import datetime, UTC
+from datetime import datetime
 
 
 from django.contrib import admin
+from django.db.models import Q
 from django.utils.html import format_html
 from django.urls import reverse
 
@@ -14,35 +15,35 @@ class IsUpcomingFilter(admin.SimpleListFilter):
     parameter_name = "is_upcoming"
 
     def lookups(self, request, model_admin):
-        now = datetime.now(UTC)
+        now = datetime.utcnow()
         qs = model_admin.get_queryset(request)
+
         if qs.filter(start_date__gt=now).exists():
             yield "Yes", "Yes"
         if qs.filter(start_date__lt=now).exists():
             yield "No", "No"
 
     def queryset(self, request, queryset):
-        value = self.value()
-        if value is not None:
-            now = datetime.now(UTC)
+        if value := self.value():
+            now = datetime.utcnow()
+
             if value == "Yes":
                 return queryset.filter(start_date__gt=now)
-            elif value == "No":
+            else:
                 return queryset.filter(start_date__lt=now)
+
+        return queryset
 
 
 @admin.register(Event, site=agency_admin_site)
 class EventAdmin(admin.ModelAdmin):
-    list_display = ["title", "start_date", "end_date", "is_upcoming", "create_tour_link"]
+    list_display = ["title", "start_date", "end_date", "create_tour_link"]
     list_filter = ["start_date", "end_date", IsUpcomingFilter]
     fields = [("title", "subtitle"), ("start_date", "end_date")]
     search_fields = ["title"]
 
-    def is_upcoming(self, obj):
-        return obj.start_date > datetime.now(UTC)
-
-    @admin.display()
-    def create_tour_link(self, obj):
+    @staticmethod
+    def create_tour_link(obj):
         url = reverse("agency_admin:agency_tour_add")
         url += f"?event={obj.id}"
         return format_html("<a href='{}'>Create Tour</a>", url)
