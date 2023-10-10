650ec765f31141fb84f52c254add94251d400c1b
Joao Righetto <jm_zr@hotmail.com>
2023-10-10 01:13:33 -0300

    refactor: change the get_objects_for_user call to the updated for_user version

--- agency/views.py
+++ agency/views.py
@@ -17,7 +17,7 @@ class TourViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.DjangoObjectPermissions | IsReadOnly]
 
     def perform_create(self, serializer):
-        if agency := Agency.objects.get_objects_for_user(self.request.user).first():
+        if agency := Agency.objects.for_user(self.request.user).first():
             serializer.save(agency=agency)
 
         # TODO: Deal with the case when this function is called, but the user has no agency.
