41d18623bb233e4d080fcf7472446032fba36a97
Joao Righetto <jm_zr@hotmail.com>
2023-10-11 22:07:31 -0300

    feat: include tickets in serialization of a Waypoint, displaying a list of passengers names for each Waypoint

--- trip/serializers.py
+++ trip/serializers.py
@@ -1,21 +1,27 @@
 from compat.django_rest_framework import serializers
 
+from sales.serializers import TicketSerializer
 from geo.serializers import PlaceSerializer
 from .models import Trip, Waypoint
 
 
+_TicketPassengersNestedSerializer = TicketSerializer.with_meta(fields=['passenger'])
+
+
 class WaypointSerializer(serializers.ModelSerializer):
-    place_id = PlaceSerializer(source="place")
+    place_id = PlaceSerializer(source='place')
+    waypoint_passenger_count = serializers.IntegerField(read_only=True)
+    tickets = _TicketPassengersNestedSerializer(many=True, read_only=True)
 
     class Meta:
         model = Waypoint
-        exclude = ["trip", "id", "order", "place"]
+        exclude = ['trip', 'id', 'order', 'place']
 
     def to_representation(self, instance):
         """Overrides Waypoint representation to include flattened Place attributes."""
         representation = super().to_representation(instance)
 
-        place_representation = representation.pop("place_id")
+        place_representation = representation.pop('place_id')
         for field_name in place_representation:
             representation[field_name] = place_representation[field_name]
 
@@ -24,7 +30,8 @@ class WaypointSerializer(serializers.ModelSerializer):
 
 class TripSerializer(serializers.WriteableNestedModelSerializer):
     route = WaypointSerializer(many=True, allow_empty=False)
+    trip_passenger_count = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = Trip
-        fields = "__all__"
+        fields = '__all__'
