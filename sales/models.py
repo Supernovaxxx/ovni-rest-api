6bd36609333457df38c95cbdce7fbc9334c8aee4
Joao Righetto <jm_zr@hotmail.com>
2023-10-13 11:53:10 -0300

    refactor: change DateField to DateTimeField

--- sales/models.py
+++ sales/models.py
@@ -15,7 +15,7 @@ class Order(models.Model):
 
     buyer = models.ForeignKey(User, on_delete=models.CASCADE)
     value = models.DecimalField(max_digits=10, decimal_places=2)
-    emission_date = models.DateField(auto_now_add=True)
+    emission_date = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=50, choices=Types.choices, default='PENDING')
 
 
