92acb9f099f22fe1d945182e6992a07259c939c7
Joao Righetto <jm_zr@hotmail.com>
2023-10-15 22:56:49 -0300

    feat: include Tickets serialization inside Orders serialization

--- sales/serializers.py
+++ sales/serializers.py
@@ -1,9 +1,19 @@
 from compat.django_rest_framework import serializers
 
-from .models import Ticket
+from .models import Ticket, Order
 
 
 class TicketSerializer(serializers.ModelSerializer):
+    passenger = serializers.SlugRelatedField(read_only=True, slug_field='username')
+
     class Meta:
         model = Ticket
+        exclude = ['order', 'id']
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    tickets = TicketSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Order
         fields = '__all__'
