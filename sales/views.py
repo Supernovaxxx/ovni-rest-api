2d673ce1ceddea52db3a0325f0c62b97e4124c64
Joao Righetto <jm_zr@hotmail.com>
2023-10-16 22:37:50 -0300

    feat: create OrderViewSet and endpoint

---
+++ sales/views.py
@@ -0,0 +1,11 @@
+import json
+
+from rest_framework import viewsets
+
+from .models import Order
+from .serializers import OrderSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
