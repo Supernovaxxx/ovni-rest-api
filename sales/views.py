f1342bc487037d3a7717eaeae456c6a5ece2687a
Joao Righetto <jm_zr@hotmail.com>
2023-10-18 00:39:06 -0300

    feat: enable tour filtering through url query strings

--- sales/views.py
+++ sales/views.py
@@ -1,4 +1,4 @@
-import json
+from django.core.exceptions import FieldError
 
 from rest_framework import viewsets
 
@@ -9,3 +9,20 @@ from .serializers import OrderSerializer
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        qs = Order.objects.all()
+
+        if params := self.request.query_params:
+            filters = {}
+            for param, value in params.items():
+                filters[param] = value
+
+            if filters:
+                try:
+                    qs = qs.filter(**filters)
+                except FieldError as e:
+                    raise e
+
+        return qs
+
