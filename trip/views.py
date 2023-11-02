f405a74fa04651fbc35bb5483a16d002bf457b20
Joao Righetto <jm_zr@hotmail.com>
2023-11-02 20:02:06 -0300

    feat: define the AgencyRelatedModelViewSet

--- trip/views.py
+++ trip/views.py
@@ -1,11 +1,33 @@
-from rest_framework import viewsets, permissions
+from rest_framework.exceptions import PermissionDenied
+
+from agency.views import AgencyRelatedModelViewSet
+
+from agency.models import Tour
+from agency.utils import get_agency_for_user
 
 from .models import Trip
 from .serializers import TripSerializer
-from authentication.permissions import IsReadOnly
 
 
-class TripViewSet(viewsets.ModelViewSet):
+class TripViewSet(AgencyRelatedModelViewSet):
     queryset = Trip.objects.all()
     serializer_class = TripSerializer
-    permission_classes = [permissions.DjangoObjectPermissions | IsReadOnly]
+
+    def perform_create(self, serializer):
+        tour_id = self.request.data.get('tour')
+
+        if tour := Tour.objects.filter(id=tour_id, agency=get_agency_for_user(self.request.user)).first():
+            serializer.save(tour=tour)
+        else:
+            raise PermissionDenied('You do not have permission to create a Trip for this Tour.')
+
+    def perform_update(self, serializer):
+        tour_id = self.request.data.get('tour')
+
+        if tour := Tour.objects.filter(id=tour_id, agency=get_agency_for_user(self.request.user)).first():
+            serializer.save(tour=tour)
+        else:
+            raise PermissionDenied('You do not have permission to create a Trip for this Tour.')
+
+
+
