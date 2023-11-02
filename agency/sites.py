17430dcc42f5452be8abe48aa4738e7a2a99a212
Joao Righetto <jm_zr@hotmail.com>
2023-11-02 13:44:13 -0300

    refactor: implement the usage of the newly created utils for retrieving the user's agency

--- agency/sites.py
+++ agency/sites.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 
+from .utils import get_agency_for_user
+
 
 class AgencyAdminSite(admin.AdminSite):
     site_title = "Agency site admin"
     site_header = "Agency administration"
 
     def has_permission(self, request):
-        from agency.models import Agency
-        return Agency.objects.for_user(request.user).exists()
+        return get_agency_for_user(request.user).exists()
 
 
 agency_admin_site = AgencyAdminSite(name="agency_admin")
