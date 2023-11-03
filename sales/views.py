162c7f87cce3d3db4b17b6ea794ccf3a5aec0b8c
Joao Righetto <jm_zr@hotmail.com>
2023-11-03 01:14:34 -0300

    feat: add queryset filtering against user's agency

--- sales/views.py
+++ sales/views.py
@@ -7,3 +7,6 @@ from .serializers import OrderSerializer
 class OrderViewSet(PrivateAgencyRelatedModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        return super().get_queryset().for_user(self.request.user)
